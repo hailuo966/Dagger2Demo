65cc42230677fd64220745d7e5e60b321eb0a679
hailuo966 <liuyanyu@100tal.com>
2018-05-10 18:05:10 +0800

    add MainActivity注释

--- app/src/main/java/demo/lyy/MainActivity.kt
+++ app/src/main/java/demo/lyy/MainActivity.kt
@@ -15,11 +15,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //普通注入，注意一个具体的实现
         Test()
+
+        //通过module注入
         Test1()
+
+        //通过module、name注入一个基类的子类实例，子类类型依据name
         Test2()
+
+        //通过module、@Qualifier注入一个基类的子类实例，子类类型依据@Qualifier
         Test3()
+
+        //通过module、dependencies注入
         Test4()
+
+        //通过module、Subcomponent注入
         Test5()
+
+        //注释不是很多，原理和用法也没有过多的讲解，可以结合 https://www.jianshu.com/p/24af4c102f62 和本Demo进行研究
+        //还有几个知识点没有做Demo，@Scope，@MapKey，Lazy，后续抽时间补充
     }
 }
